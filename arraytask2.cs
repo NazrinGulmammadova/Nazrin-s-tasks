02e5cfed93e9c37a333717880c5d2cebb89789e3
NazrinGulmammadova <116623519+NazrinGulmammadova@users.noreply.github.com>
2022-11-02 00:57:17 +0400

    created tasks

---
+++ arraytask2.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = { 5, 10, 12, 14, 15, 9 };
+int sum = 0;
+int avarage = 0;
+int num;
+for (int index = 0; index < arr.Length; index++)
+{
+    if (num == arr[index])
+    {
+        sum = sum + num;
+        avarage = sum / arr.Length;
+    }
+
+
+}
+    Console.WriteLine(avarage);
---
+++ arraytask3.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = { -2, 4, 6, 8 };
+int countPositive = 0;
+int countNegative = 0;
+for (int i=0; i<arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        countPositive++;
+    }
+    else if (arr[i] < 0)
+    {
+        countNegative++;
+    }
+}
+Console.WriteLine(countPositive);
+Console.WriteLine(countNegative); 
